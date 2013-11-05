4e655591e7eeb1a3ec6f58b2901584b29d1132e4
mzimen <mzimen@redhat.com>
2013-11-05 09:33:21 +0100

    Fixed missing TESTRUN_ID

--- jenkins.js
+++ jenkins.js
@@ -11,7 +11,10 @@ function txtswap(){
 
   //find the testrun ID
   var found=body_html.match(/TESTRUN_ID=(\d+)/);
-  var testrun_id=found[1];
+  var testrun_id=null;
+  if (found !=null && found.length>0)
+    testrun_id=found[1];
+
 
   /*Replace the <pre> tag with an accordion div*/
   var accordion_replace_prefix = body_html.replace(/<span style="display: none;[^<]+<\/span>/g,'');
@@ -21,7 +24,8 @@ function txtswap(){
   //we can assume that every 6digits are TestCases...
   accordion_replace_prefix = accordion_replace_prefix.replace(/ (\d{6}) /g,'<a target="_blank" href="https://tcms.engineering.redhat.com/case/$1">$1</a>');
   //we can assume that every 6digits are TestCaseRuns...
-  accordion_replace_prefix = accordion_replace_prefix.replace(/ (\d{7}) /g,'<a target="_blank" href="https://tcms.engineering.redhat.com/run/'+testrun_id+'/#caserun_$1">$1</a>');
+  if (testrun_id)
+      accordion_replace_prefix = accordion_replace_prefix.replace(/ (\d{7}) /g,'<a target="_blank" href="https://tcms.engineering.redhat.com/run/'+testrun_id+'/#caserun_$1">$1</a>');
   document.body.innerHTML = accordion_replace_prefix;
 
   var scenario_replace = document.getElementById("accordion").innerHTML.replace(/Scenario(?: Outline)?: ([^#]+)/g,
